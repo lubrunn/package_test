040035cccb16cf2562e66fc40be3cf801bcbe4ed
Lukas Brunner <lukas_brunner95@yahoo.de>
2021-03-13 00:37:45 +0100

    Revert "inst"
    This reverts commit 018d6145216b6e47cad2b96416686fa35beba3e3.

--- www/rm_network.js
+++
@@ -1,6 +0,0 @@
-$("#reset_net").click(function() {
-  $(".display.forceNetwork.no-footer").ForceNetwork().destroy();
-  $(".display.forceNetwork.no-footer").ForceNetwork().clear().draw();    
-  $(".display.no-footer").ForceNetwork().destroy();
-  $(".display.no-footer").ForceNetwork().clear().draw();    
-});
